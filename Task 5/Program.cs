875eae291c9d5a565766756859e8094dad5a92cf
Webprivstu <beggs-g4@ulster.ac.uk>
2025-09-28 00:23:20 +0100

    Add project files.

---
+++ Task 5/Program.cs
@@ -0,0 +1,21 @@
+﻿int number;
+
+void Checker()
+{
+
+    Console.WriteLine("Enter an integer");
+    number = Convert.ToInt32(Console.ReadLine());
+
+    if (number % 2 == 0)
+    {
+
+        Console.WriteLine("Your number is even");
+    }
+    else
+    {
+        Console.WriteLine("Your number is odd");
+
+    }
+}
+
+Checker();
