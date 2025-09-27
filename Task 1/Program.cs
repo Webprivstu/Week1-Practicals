feefa87d6e72761415ca1b535111e8f3dbf87192
Webprivstu <beggs-g4@ulster.ac.uk>
2025-09-27 23:16:10 +0100

    task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name;
+int age;
+
+Console.WriteLine("Please enter your name");
+
+name = Console.ReadLine();
+
+Console.WriteLine("Enter your age");
+
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Hello {name}, you will be {(age + 5)} in five years ");
