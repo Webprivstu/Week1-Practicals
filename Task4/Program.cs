385c764563ff75abea518f4ae0b9b67fea14bca8
Webprivstu <beggs-g4@ulster.ac.uk>
2025-09-28 00:18:30 +0100

    Add project files.

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.Design;
+
+int year;
+void LeapYearChecker()
+{
+
+    Console.WriteLine("Enter a year");
+    year = Convert.ToInt32(Console.ReadLine());
+
+
+
+    if ( year % 4 == 0 || year % 400 == 0 ) {
+
+        Console.WriteLine($"{year} is a leap year");
+
+    } else if ( year > 0 )
+    {
+        Console.WriteLine($"{year} is not a leap year");
+    } else
+    {
+        Console.WriteLine("Invalid input");
+    }
+
+}
+
+LeapYearChecker();
