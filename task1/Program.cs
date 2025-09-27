b634fd815ab2b3da8d3f89c139399c439269f2c9
Webprivstu <beggs-g4@ulster.ac.uk>
2025-09-27 23:44:32 +0100

    task 2

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices.Marshalling;
+
+void AgeFigureOuter() {
+
+int age;
+
+Console.WriteLine("What is your age?");
+
+age = Convert.ToInt32(Console.ReadLine());
+
+    if (age > 19 && age < 110) {
+
+    Console.WriteLine("You are an adult");
+
+    }
+    else if (age > 0 && age < 13) {
+        Console.WriteLine("You are a child");
+
+    } else if ((age <= 19) && (age <= 13) && (age !=0))
+    {
+        Console.WriteLine("You are a teenager");
+    } else 
+    {
+        Console.WriteLine("Invalid option - either you don't exist or your are dead. Sorry!");
+    }
+
+}
+
+AgeFigureOuter();
