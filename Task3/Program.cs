da86f3f4e88c888252209a12d86f8a52ab3c4244
Webprivstu <beggs-g4@ulster.ac.uk>
2025-09-27 23:56:13 +0100

    works

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int number;
+
+void Checker()
+{
+
+    Console.WriteLine("Enter an integer");
+    number = Convert.ToInt32(Console.ReadLine());
+
+    if (number < 0) {
+
+        Console.WriteLine("Your number is negative");
+
+    }
+    else if (number >= 0) {
+
+        Console.WriteLine("Your number is positive");
+    }
+
+
+}
+
+Checker();
